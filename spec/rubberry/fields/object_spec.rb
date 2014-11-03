79179dcc3e85ac4d0c212220824e61544fa6af6c
undr <undr@yandex.ru>
2014-11-03 18:33:02 +0700

    Fix for rbx version `Time.at`
    ruby: Time.at(1414571405.123).usec # => 123000
    rbx:  Time.at(1414571405.123).usec # => 122999

--- spec/rubberry/fields/object_spec.rb
+++ spec/rubberry/fields/object_spec.rb
@@ -13,7 +13,7 @@ describe Rubberry::Fields::Object do
     end
   end
 
-  describe '#type_cast', time_freeze: '2014-10-29T15:30:05.123+07:00' do
+  describe '#type_cast', time_freeze: '2014-10-29T08:30:05.124' do
     specify{ expect(subject.type_cast(nil)).to be_nil }
     specify{ expect(subject.type_cast({})).to eq(OpenStruct.new(title: nil, created_at: nil, author: nil)) }
     specify{ expect(subject.type_cast(
@@ -25,7 +25,7 @@ describe Rubberry::Fields::Object do
     )).to eq(OpenStruct.new(title: 'Title', created_at: Time.now.to_datetime, author: OpenStruct.new(name: 'Undr'))) }
   end
 
-  describe '#elasticize', time_freeze: '2014-10-29T15:30:05.123+07:00' do
+  describe '#elasticize', time_freeze: '2014-10-29T08:30:05.124' do
     specify{ expect(subject.type_cast(nil).elasticize).to be_nil }
     specify{ expect(subject.type_cast({}).elasticize).to eq('title' => nil, 'created_at' => nil, 'author' => nil) }
     specify{ expect(subject.type_cast(
@@ -35,7 +35,7 @@ describe Rubberry::Fields::Object do
     specify{ expect(subject.type_cast(
       'title' => 'Title', 'last' => 'Great', 'created_at' => Time.now, 'author' => { name: 'Undr' }
     ).elasticize).to eq(
-      'title' => 'Title', 'created_at' => '2014-10-29T08:30:05.123', 'author' => { 'name' => 'Undr' }
+      'title' => 'Title', 'created_at' => '2014-10-29T08:30:05.124', 'author' => { 'name' => 'Undr' }
     ) }
   end
 end
